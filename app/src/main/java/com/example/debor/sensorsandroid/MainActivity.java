1c6ee6b302c6e8a79a080345cf6a40ef548891b1
Natasha S Love <debora@inbox.ru>
2018-11-28 18:50:07 -0600

    Working at last! After commenting out 2 methods in MAin

--- app/src/main/java/com/example/debor/sensorsandroid/MainActivity.java
+++ app/src/main/java/com/example/debor/sensorsandroid/MainActivity.java
@@ -12,9 +12,10 @@ import android.widget.TextView;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    TextView tv1=null;
+    TextView tv1 = null;
     private SensorManager mSensorManager;
-//HOW TO RESOLVE 'R'?
+
+    //HOW TO RESOLVE 'R'?
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -25,14 +26,18 @@ public class MainActivity extends AppCompatActivity {
         tv1.setVisibility(View.GONE);
 
         mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        List<Sensor> mList= mSensorManager.getSensorList(Sensor.TYPE_ALL);
+        List<Sensor> mList = mSensorManager.getSensorList(Sensor.TYPE_ALL);
 
         for (int i = 1; i < mList.size(); i++) {
             tv1.setVisibility(View.VISIBLE);
             tv1.append("\n" + mList.get(i).getName() + "\n" + mList.get(i).getVendor() + "\n" + mList.get(i).getVersion());
-        }
-    }
 
+            //sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+            // List<Sensor> mList = sensorManager.getSensorList(Sensor.TYPE_ALL);
+            }
+        }
+
+    /*
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -54,4 +59,6 @@ public class MainActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
-}
+    */
+    }
+
